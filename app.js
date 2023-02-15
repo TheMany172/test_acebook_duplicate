095370ed5f378dc21706a06bcaa634ed04bc0923
Adam Little <themany@proton.me>
2023-02-15 11:43:47 +0000

    changed it back

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.use(methodOverride("_method"));
 
 app.use(
   session({
-    key: "Test_key",
+    key: "user_sid",
     secret: "super_secret",
     resave: false,
     saveUninitialized: false,
